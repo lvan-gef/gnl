ccc4e9c54a68d897e688adcbdf918bf81a80b68a
luuk <lvan-gef@student.codam.nl>
2025-02-11 23:20:43 +0100

    Function to find newline

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lvan-gef <lvan-gef@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/11 20:31:49 by lvan-gef      #+#    #+#                 */
-/*   Updated: 2025/02/11 20:31:49 by lvan-gef      ########   odam.nl         */
+/*   Updated: 2025/02/11 23:06:23 by lvan-gef      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "../include/get_next_line.h"
 
 static char *read_chunk(t_gnl *gnl);
 static void gnl_free(t_gnl *gnl);
 static char *new_line(t_gnl *gnl);
+static ssize_t find_new_line(t_gnl *gnl);
 
 char *get_next_line(int fd) {
     static t_gnl gnl = {0};
@@ -62,6 +64,7 @@ static char *read_chunk(t_gnl *gnl) {
         }
     }
 
+    char *line = NULL;
     while (true) {
         ssize_t read_size = read(gnl->fd, gnl->buf, BUF_SIZE);
         if (read_size < 0) {
@@ -69,15 +72,19 @@ static char *read_chunk(t_gnl *gnl) {
             gnl_free(gnl);
             return NULL;
         } else if (read_size == 0) {
-            // return what is in the buffer
+            return new_line(gnl);
         }
 
         gnl->buf_len += (size_t)read_size;
-        new_line(gnl);
-
-        // search for \n or \r\n
-        // if not found alloc more space and keep reading
+        line = new_line(gnl);
+        if (errno != 0) {
+            return line;
+        } else if (line) {
+            return line;
+        }
     }
+
+    return NULL; // should never get here?
 }
 
 static void gnl_free(t_gnl *gnl) {
@@ -92,7 +99,39 @@ static void gnl_free(t_gnl *gnl) {
 }
 
 static char *new_line(t_gnl *gnl) {
-    (void)gnl;
+    ssize_t new_line_pos = find_new_line(gnl);
+
+    if (new_line_pos == -1) {
+        return NULL;
+    }
+
+    // calloc space till newline
+    // copy it over from buf to line
+    // then move everything after newline over in the buffer to the beginning
+    // update buf_len
 
     return NULL;
+}
+
+static ssize_t find_new_line(t_gnl *gnl) {
+    ssize_t new_line_pos = -1;
+    size_t index = 0;
+
+    while (index < gnl->buf_len && gnl->buf[index] != '\0') {
+        if (gnl->buf[index] == '\r') {
+            if (index + 1 < gnl->buf_len) {
+                index++;
+
+                if (index + 1 == '\n') {
+                    return (ssize_t)index;
+                }
+            }
+        } else if (gnl->buf[index]) {
+            return (ssize_t)index;
+        }
+
+        index++;
+    }
+
+    return new_line_pos;
 }
